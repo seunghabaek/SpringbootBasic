f4448fec399333adf87a808c4bf2eeadd64b9954
seungha <slug0326@naver.com>
2021-10-09 00:48:33 +0900

    using JPA

---
+++ src/main/java/com/hama/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.hama.book.springboot.domain.posts;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter // lombok - Getter 메소드 자동생성
+@NoArgsConstructor // lombok - 기본 생성자 자동추가, public Posts() {}와 같은 효과
+@Entity // jpa
+public class Posts {
+
+    @Id // pk field
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // pk rule - auto increment
+    private Long id;
+
+    @Column(length = 500, nullable = false) // table column
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // lombok - 생성자와 비슷한 역할.
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/test/java/com/hama/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.hama.book.springboot.domain.posts;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll(); // repository 비우는 코드.
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder() // posts 테이블에 insert/update 쿼리 실행.
+                .title(title)
+                .content(content)
+                .author("slug0326@naver.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll(); // 테이블 posts에 있는 모든 데이터 조회
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
